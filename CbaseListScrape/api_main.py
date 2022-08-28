99064edb929a9f82fd41f9f9b92fec76f47aead7
Leon <cohenleonlbc@gmail.com>
2022-08-28 19:27:22 -0300

    duplicate try except

--- CbaseListScrape/api_main.py
+++ CbaseListScrape/api_main.py
@@ -8,12 +8,8 @@ def api_main(requests = ['crunchBaseList']):
 		req = Request(json_file)
 		if f_utils.already_saved_today(req.last_access) == False:
 			print('> Acessando a API: ' + req.api_name + '...')
-			try:
-				json_response = req.call_api()
-			except ConnectionError:
-				print('> Nao conseguiu acessar a API: '+ req.api_name + '!')
-			else:
-				f_utils.save_response(json_response, req.api_name)
+			json_response = req.call_api()
+			f_utils.save_response(json_response, req.api_name)
 		else:
 			print("> A API " + req.api_name + " ja foi acessada hoje")
 
